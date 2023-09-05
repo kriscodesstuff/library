22ac48e303f4999f2f1f06ac54534a2cf9f84d8a
Kristin <89033364+kriscodesstuff@users.noreply.github.com>
2023-09-05 17:07:53 -0500

    Create function that displays each book

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-alert('hello world')
+const container = document.querySelector('#container');
+
+function Book(title,author,pages,status){
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+
+    this.info = function(){
+        return `${title} by ${author}, ${pages} pages,${status}`;
+    }
+
+}
+
+const bookArr = [];
+
+function addBooktoLibrary(title,author,pages,status) {
+    bookArr.push(new Book(title,author,pages,status));
+   
+}
+
+addBooktoLibrary('bora','bts',235,'read');
+addBooktoLibrary('hello','me',293,'not read');
+
+bookArr.forEach((book) =>{
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('card');
+    container.appendChild(newDiv);
+
+    let ul = document.createElement('ul');
+    newDiv.appendChild(ul);
+
+    let titleLi = document.createElement('li');
+    titleLi.innerText = book.title;
+    ul.appendChild(titleLi);
+
+    let authorLi = document.createElement('li');
+    authorLi.innerText = book.author;
+    ul.appendChild(authorLi);
+
+    let pagesLi = document.createElement('li');
+    pagesLi.innerText = book.pages;
+    ul.appendChild(pagesLi);
+
+    let statusLi = document.createElement('li');
+    statusLi.innerText = book.status;
+    ul.appendChild(statusLi);
+
+})
+
