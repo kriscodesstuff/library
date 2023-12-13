b1c2bf8aa85e272294bf72e7a7b29c38c03e40ea
Kristin <89033364+kriscodesstuff@users.noreply.github.com>
2023-12-13 11:44:48 -0600

    Update displayBook function to work with class

--- script.js
+++ script.js
@@ -10,19 +10,6 @@ let deleteButtons = document.querySelectorAll('.delete-btn');
 
 
 
-// function Book(title,author,pages,status){
-
-//     this.title = title; 
-//     this.author = author;
-//     this.pages = pages;
-//     this.status = status;
-
-//     this.info = function(){
-//         return `${title} by ${author}, ${pages} pages,${status}`;
-//     }
-
-// }
-
 
 
 class Book {
@@ -56,52 +43,49 @@ newBookBtn.addEventListener('click', () => {
 
 
 
-// function displayBook(){
+function displayBook(){
 
         
-//             const newDiv = document.createElement('div');
-//             newDiv.classList.add('card');
-//             container.appendChild(newDiv);
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('card');
+            container.appendChild(newDiv);
             
-//             const bookList = document.createElement('ul');
-//             newDiv.appendChild(bookList);
+            const bookList = document.createElement('ul');
+            newDiv.appendChild(bookList);
 
-//             const bookTitle = document.createElement('li');
-//             bookTitle.innerText = `${bookArr[bookArr.length - 1].title}`;
-//             bookList.appendChild(bookTitle);
+            const bookTitle = document.createElement('li');
+            bookTitle.innerText = `${newBook.getBooks()[newBook.getBooks().length - 1].title}`;
+            bookList.appendChild(bookTitle);
 
-//             const bookAuthor = document.createElement('li');
-//             bookAuthor.innerText = `${bookArr[bookArr.length - 1].author}`;
-//             bookList.appendChild(bookAuthor);
+            const bookAuthor = document.createElement('li');
+            bookAuthor.innerText = `${newBook.getBooks()[newBook.getBooks().length - 1].author}`;
+            bookList.appendChild(bookAuthor);
 
-//             const bookPages = document.createElement('li');
-//             bookPages.innerText = `${bookArr[bookArr.length - 1].pages}`;
-//             bookList.appendChild(bookPages);
+            const bookPages = document.createElement('li');
+            bookPages.innerText = `${newBook.getBooks()[newBook.getBooks().length - 1].pages}`;
+            bookList.appendChild(bookPages);
 
-//             const bookStatus = document.createElement('li');
-//             bookStatus.innerText = `${bookArr[bookArr.length - 1].status}`;
-//             bookList.appendChild(bookStatus);
+            const bookStatus = document.createElement('li');
+            bookStatus.innerText = `${newBook.getBooks()[newBook.getBooks().length - 1].status}`;
+            bookList.appendChild(bookStatus);
 
-//             const deleteBtn = document.createElement('button');
-//             deleteBtn.innerText = 'Delete';
-//             bookList.appendChild(deleteBtn);
+            const deleteBtn = document.createElement('button');
+            deleteBtn.innerText = 'Delete';
+            bookList.appendChild(deleteBtn);
     
-//             deleteBtn.addEventListener('click', () => {
-//                 const currentDiv = deleteBtn.parentNode;
-//                 currentDiv.remove();
-//             })
+            deleteBtn.addEventListener('click', () => {
+                const currentDiv = deleteBtn.parentNode;
+                currentDiv.remove();
+            })
             
 
-//     };
+    };
 
 let newBook = new Book();
 
 submitBtn.addEventListener('click', () => {
     newBook.addBooktoLibrary(titleInput.value,authorInput.value,pagesInput.value,statusInput.value);
-console.log(newBook.getBooks());
-    // displayBook();
-    
-
+    displayBook();
 });
 
 // deleteButtons.forEach((button) => {
