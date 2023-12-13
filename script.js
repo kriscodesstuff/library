836f18f5a2c9b8819bf7e7dd373f5268a0422537
Kristin <89033364+kriscodesstuff@users.noreply.github.com>
2023-12-13 10:04:24 -0600

    Change Book  into a class

--- script.js
+++ script.js
@@ -10,17 +10,26 @@ let deleteButtons = document.querySelectorAll('.delete-btn');
 
 
 
-function Book(title,author,pages,status){
+// function Book(title,author,pages,status){
 
-    this.title = title; 
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
+//     this.title = title; 
+//     this.author = author;
+//     this.pages = pages;
+//     this.status = status;
 
-    this.info = function(){
-        return `${title} by ${author}, ${pages} pages,${status}`;
+//     this.info = function(){
+//         return `${title} by ${author}, ${pages} pages,${status}`;
+//     }
+
+// }
+
+class Book {
+    constructor(title,author,pages,status) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
     }
-
 }
 
 
